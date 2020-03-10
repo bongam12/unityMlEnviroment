951db4db7c5a1258ac9f958d5f2c283bfdda10e2
bongam12 <55765653+bongam12@users.noreply.github.com>
2020-03-10 11:52:23 -0500

    Update gameManager.cs
    2 person manager

--- gameManager.cs
+++ gameManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,17 +8,16 @@ public class gameManager : MonoBehaviour
 {
     [Header("References")]
     public carControl controller;
+    public carControl2 player2;
+
     [Header("Controls")]
     public int initialPopulation = 85;
     [Range(0.0f, 1.0f)]
-    //chance population will be randomized
     public float mutationRate = 0.055f;
 
     [Header("Crossover Controls")]
-    //picking 8 of best cars and 3 of worst cars
     public int bestAgentSelection = 8;
     public int worstAgentSelection = 3;
-    //number of pop to take weights
     public int numberToCrossover;
 
     private List<int> genePool = new List<int>();
@@ -26,11 +25,13 @@ public class gameManager : MonoBehaviour
     private int naturallySelected;
 
     private reinforcementNet[] population;
-    //low generations for performance is better algo
+    private reinforcement2Child[] population2;
+
     [Header("Public View")]
-    //debugging purposes
     public int currentGeneration;
     public int currentGenome = 0;
+    public int currentGeneration2;
+    public int currentGenome2 = 0;
 
     private void Start()
     {
@@ -39,78 +40,103 @@ public class gameManager : MonoBehaviour
 
     private void CreatePopulation()
     {
-        //car population for first gene pool
         population = new reinforcementNet[initialPopulation];
-        //fill population with random values
-        FillPopulationWithRandomValues(population, 0);
-        //reset current the current car of the population
+        population2 = new reinforcement2Child[initialPopulation];
+        FillPopulationWithRandomValues(population,population2, 0);
         ResetToCurrentGenome();
+
+        //second person
+        
+        
     }
 
     private void ResetToCurrentGenome()
     {
         controller.ResetWithNetwork(population[currentGenome]);
+        player2.ResetWithNetwork(population2[currentGenome2]);
     }
-    //later set starting index to end because all nets up to that point have been
-    //created with other nets
-    private void FillPopulationWithRandomValues(reinforcementNet[] newPopulation, int startingIndex)
+
+    private void FillPopulationWithRandomValues(reinforcementNet[] newPopulation, reinforcement2Child[] newPopulation2, int startingIndex)
     {
         while (startingIndex < initialPopulation)
         {
             newPopulation[startingIndex] = new reinforcementNet();
             newPopulation[startingIndex].Initialise(controller.LAYERS, controller.NEURONS);
+            //second person
+            newPopulation2[startingIndex] = new reinforcement2Child();
+            newPopulation2[startingIndex].Initialise2(player2.LAYERS, player2.NEURONS);
             startingIndex++;
         }
     }
-    //this helps improve the performance of current genome
+
     public void Death(float fitness, reinforcementNet network)
     {
+       
 
         if (currentGenome < population.Length - 1)
         {
-            //set the previous fitness from the death of the car controller
-            //fitness then stored in dec order
+
             population[currentGenome].fitness = fitness;
+            Debug.Log("death first layer");
             currentGenome++;
-            //reset car with different net but saved fitness
+            Debug.Log("death active");
             ResetToCurrentGenome();
+            
 
         }
         else
         {
-            //repopulate current generation
             RePopulate();
         }
 
+
+    }
+    public void Death2(float fitness2,reinforcement2Child network2)
+    {
+        if (currentGenome2 < population2.Length - 1)
+        {
+
+            population2[currentGenome2].fitness = fitness2;
+            Debug.Log("death first layer");
+            currentGenome2++;
+            Debug.Log("death active");
+            ResetToCurrentGenome();
+
+
+        }
+        else
+        {
+            RePopulate();
+        }
     }
 
-    //mor management oriented
     private void RePopulate()
     {
-        //clear generation
         genePool.Clear();
         currentGeneration++;
+        currentGeneration2++;
         naturallySelected = 0;
-        //sort popluation by fitness
         SortPopulation();
-        //also picking 3 of worst
-        reinforcementNet[] newPopulation = PickBestPopulation();
 
-        Crossover(newPopulation);
-        Mutate(newPopulation);
-        //start at naturally selected instead of zero and go through all
-        //nets that havent been initialized
-        FillPopulationWithRandomValues(newPopulation, naturallySelected);
+        reinforcementNet[] newPopulation = PickBestPopulation();
+        reinforcement2Child[] newPopulation2 = PickBestPopulation2();
+
+        Crossover(newPopulation,newPopulation2);
+        Mutate(newPopulation,newPopulation2);
+
+        FillPopulationWithRandomValues(newPopulation,newPopulation2, naturallySelected);
 
         population = newPopulation;
+        population2 = newPopulation2;
 
         currentGenome = 0;
+        currentGenome2 = 0;
 
         ResetToCurrentGenome();
 
     }
 
-    private void Mutate(reinforcementNet[] newPopulation)
+    private void Mutate(reinforcementNet[] newPopulation, reinforcement2Child[] newPopulation2)
     {
 
         for (int i = 0; i < naturallySelected; i++)
@@ -118,10 +144,11 @@ public class gameManager : MonoBehaviour
 
             for (int c = 0; c < newPopulation[i].weights.Count; c++)
             {
-                //mutate a % of populations weights --> randomizes its matrix
+
                 if (Random.Range(0.0f, 1.0f) < mutationRate)
                 {
                     newPopulation[i].weights[c] = MutateMatrix(newPopulation[i].weights[c]);
+                    newPopulation2[i].weights[c] = MutateMatrix(newPopulation[i].weights[c]);
                 }
 
             }
@@ -132,19 +159,16 @@ public class gameManager : MonoBehaviour
 
     Matrix<float> MutateMatrix(Matrix<float> A)
     {
-        //do not mutate all of the weights mutate some of the weights
-        //pick random amount of values in matrix
-        // /7 seems to work well
+
         int randomPoints = Random.Range(1, (A.RowCount * A.ColumnCount) / 7);
 
         Matrix<float> C = A;
 
         for (int i = 0; i < randomPoints; i++)
         {
-            //change by random position column and row 
             int randomColumn = Random.Range(0, C.ColumnCount);
             int randomRow = Random.Range(0, C.RowCount);
-            //instead of setting new value bump it up or down by a bit
+
             C[randomRow, randomColumn] = Mathf.Clamp(C[randomRow, randomColumn] + Random.Range(-1f, 1f), -1f, 1f);
         }
 
@@ -152,27 +176,20 @@ public class gameManager : MonoBehaviour
 
     }
 
-    private void Crossover(reinforcementNet[] newPopulation)
+    private void Crossover(reinforcementNet[] newPopulation, reinforcement2Child[] newPopulation2)
     {
-        //cross over based on gene pool
-        //increment by two
         for (int i = 0; i < numberToCrossover; i += 2)
         {
-            //first parent index
             int AIndex = i;
-            //second parent
             int BIndex = i + 1;
 
             if (genePool.Count >= 1)
             {
                 for (int l = 0; l < 100; l++)
                 {
-                    
-                    //find random elemnt in gene pool
                     AIndex = genePool[Random.Range(0, genePool.Count)];
                     BIndex = genePool[Random.Range(0, genePool.Count)];
-                    //ensure parents are not the same
-                    //we found what we looking for break
+
                     if (AIndex != BIndex)
                         break;
                 }
@@ -180,33 +197,46 @@ public class gameManager : MonoBehaviour
 
             reinforcementNet Child1 = new reinforcementNet();
             reinforcementNet Child2 = new reinforcementNet();
+            //
+            reinforcement2Child Child1_A = new reinforcement2Child();
+            reinforcement2Child Child2_A = new reinforcement2Child();
 
             Child1.Initialise(controller.LAYERS, controller.NEURONS);
             Child2.Initialise(controller.LAYERS, controller.NEURONS);
 
+            Child1_A.Initialise2(player2.LAYERS, player2.NEURONS);
+            Child2_A.Initialise2(player2.LAYERS, player2.NEURONS);
+
             Child1.fitness = 0;
             Child2.fitness = 0;
 
-            //should be more subtle in practice to much transfer of info
+            Child1_A.fitness = 0;
+            Child2_A.fitness = 0;
+
+
             for (int w = 0; w < Child1.weights.Count; w++)
             {
 
                 if (Random.Range(0.0f, 1.0f) < 0.5f)
                 {
-                    //taje weights from parents
                     Child1.weights[w] = population[AIndex].weights[w];
                     Child2.weights[w] = population[BIndex].weights[w];
+                    //second person
+                    Child2_A.weights[w] = population2[AIndex].weights[w];
+                    Child1_A.weights[w] = population2[BIndex].weights[w];
                 }
                 else
                 {
-                    //random chance for kids
                     Child2.weights[w] = population[AIndex].weights[w];
                     Child1.weights[w] = population[BIndex].weights[w];
+                    //second person
+                    Child1_A.weights[w] = population2[AIndex].weights[w];
+                    Child2_A.weights[w] = population2[BIndex].weights[w];
                 }
 
             }
 
-            //indv values instead of matices
+
             for (int w = 0; w < Child1.biases.Count; w++)
             {
 
@@ -214,20 +244,30 @@ public class gameManager : MonoBehaviour
                 {
                     Child1.biases[w] = population[AIndex].biases[w];
                     Child2.biases[w] = population[BIndex].biases[w];
+                    //second person
+                    Child2_A.biases[w] = population2[AIndex].biases[w];
+                    Child1_A.biases[w] = population2[BIndex].biases[w];
                 }
                 else
                 {
                     Child2.biases[w] = population[AIndex].biases[w];
                     Child1.biases[w] = population[BIndex].biases[w];
+                    //second person
+                    Child1_A.biases[w] = population2[AIndex].biases[w];
+                    Child2_A.biases[w] = population2[BIndex].biases[w];
                 }
 
             }
-            //add to new population
+
             newPopulation[naturallySelected] = Child1;
+            newPopulation2[naturallySelected] = Child1_A;
             naturallySelected++;
-            //add to new population
+
             newPopulation[naturallySelected] = Child2;
+            newPopulation2[naturallySelected] = Child2_A;
             naturallySelected++;
+
+            
 
         }
     }
@@ -236,25 +276,17 @@ public class gameManager : MonoBehaviour
     {
 
         reinforcementNet[] newPopulation = new reinforcementNet[initialPopulation];
-        //diverse gene pool to make higher chance of top network over lower network
+
         for (int i = 0; i < bestAgentSelection; i++)
         {
-            //copy is there because without ypu would be changine the refernce population i
-            //passing over the best networks
-            //increments number of filled nets
-            //being added to new pop
             newPopulation[naturallySelected] = population[i].InitialiseCopy(controller.LAYERS, controller.NEURONS);
             newPopulation[naturallySelected].fitness = 0;
-            //the remaining non selected have rabdomized values
             naturallySelected++;
-            //take fitness, multiply by ten and add to gene pool
-            //ie survival of the fittest
+
             int f = Mathf.RoundToInt(population[i].fitness * 10);
 
             for (int c = 0; c < f; c++)
             {
-                //adding indexes to reference neural net
-                //example newpopulation[genepool[i]] == neural net
                 genePool.Add(i);
             }
 
@@ -262,11 +294,10 @@ public class gameManager : MonoBehaviour
 
         for (int i = 0; i < worstAgentSelection; i++)
         {
-            //loop from bottom of array
             int last = population.Length - 1;
             last -= i;
 
-            int f = Mathf.RoundToInt(population[last].fitness * 8);
+            int f = Mathf.RoundToInt(population[last].fitness * 10);
 
             for (int c = 0; c < f; c++)
             {
@@ -279,19 +310,63 @@ public class gameManager : MonoBehaviour
 
     }
 
+    private reinforcement2Child[] PickBestPopulation2()
+    {
+
+        reinforcement2Child[] newPopulation2 = new reinforcement2Child[initialPopulation];
+
+        for (int i = 0; i < bestAgentSelection; i++)
+        {
+            newPopulation2[naturallySelected] = population2[i].InitialiseCopy2(player2.LAYERS, player2.NEURONS);
+               
+
+            newPopulation2[naturallySelected].fitness = 0;
+            naturallySelected++;
+
+            int f = Mathf.RoundToInt(population[i].fitness * 10);
+
+            for (int c = 0; c < f; c++)
+            {
+                genePool.Add(i);
+            }
+
+        }
+
+        for (int i = 0; i < worstAgentSelection; i++)
+        {
+            int last = population.Length - 1;
+            last -= i;
+
+            int f = Mathf.RoundToInt(population[last].fitness * 10);
+
+            for (int c = 0; c < f; c++)
+            {
+                genePool.Add(last);
+            }
+
+        }
+
+        return newPopulation2;
+
+    }
+
     private void SortPopulation()
     {
-        //i might rework this
         for (int i = 0; i < population.Length; i++)
         {
             for (int j = i; j < population.Length; j++)
             {
                 if (population[i].fitness < population[j].fitness)
                 {
-                    //order from high to low fitness
                     reinforcementNet temp = population[i];
                     population[i] = population[j];
                     population[j] = temp;
+                }
+                if (population2[i].fitness < population2[j].fitness)
+                {
+                    reinforcement2Child temp2 = population2[i];
+                    population2[i] = population2[j];
+                    population2[j] = temp2;
                 }
             }
         }
